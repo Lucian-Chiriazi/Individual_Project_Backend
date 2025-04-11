03efc2028024af33bbeb3b694125b6432813f0a3
Lucian Chiriazi <153869152+Lucian-Chiriazi@users.noreply.github.com>
2025-04-11 18:54:13 +0100

    Change cors moddleware to allow clowd deployment

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ app = FastAPI()
 # Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["https://lucian-chiriazi.github.io", "http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
